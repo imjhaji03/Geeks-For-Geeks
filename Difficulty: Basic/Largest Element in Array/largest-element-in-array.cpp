3f5f3d5376650ba0bc5f3542f186c41a2a778f76
Amit Gunjan Jha <119215172+imjhaji03@users.noreply.github.com>
2025-06-12 12:05:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Largest Element in Array/largest-element-in-array.cpp
+++ Difficulty: Basic/Largest Element in Array/largest-element-in-array.cpp
@@ -1,58 +1,15 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// User function Template for C++
-
 class Solution {
   public:
-   
     int largest(vector<int> &arr) {
+        // code here
         int n = arr.size();
-        int max = arr[0];
+        int res = arr[0];
         for(int i = 0; i < n; i++){
-            if(arr[i] > arr[0]){
-                arr[0] = arr[i];
+            if(arr[i] > res){
+                res = arr[i];
             }
         }
-        return arr[0];
+        return res;
         
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    cin.ignore();
-
-    while (t--) {
-        string input;
-        int num;
-        vector<int> arr;
-
-        getline(cin, input);
-        stringstream s2(input);
-        while (s2 >> num) {
-            arr.push_back(num);
-        }
-
-        Solution ob;
-        cout << ob.largest(arr) << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
