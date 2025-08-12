c8915495c6d14adda3e27492bd6c1bf2428d0f32
Amit Gunjan Jha <119215172+imjhaji03@users.noreply.github.com>
2025-08-12 10:31:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smaller on Left/smaller-on-left.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    vector<int> leftSmaller(vector<int> arr) {
+        // code here
+        vector<int> v;
+        stack <int> s;
+        int n = arr.size();
+        
+        for(int i = 0; i < n; i++){
+            if(s.size() == 0){
+                v.push_back(-1);
+            }
+            else if(s.size() != 0 && s.top() < arr[i]){
+                v.push_back(s.top());
+            }
+            else if(s.size() != 0 && s.top() >= arr[i]){
+                while(s.size() != 0 && s.top() >= arr[i]){
+                    s.pop();
+                }
+                if(s.size() == 0){
+                    v.push_back(-1);
+                }
+                else{
+                    v.push_back(s.top());
+                }
+            }
+            s.push(arr[i]);
+        }
+        return v;
+    }
+};
