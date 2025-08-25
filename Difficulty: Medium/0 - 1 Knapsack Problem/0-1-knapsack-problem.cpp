2fd361979caf82067b17cdc5a713a0b283e0de60
Amit Gunjan Jha <119215172+imjhaji03@users.noreply.github.com>
2025-08-25 18:41:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,31 +1,29 @@
 class Solution {
   public:
   
-    int knapsackRec(int W, vector<int> &val, vector<int> &wt, int n, vector<vector<int>> &t){
-        if(W == 0 || n == 0){
+    int knapsackrec(int W, vector<int> &val, vector<int> &wt, int n, vector<vector<int>> &t){
+        if(n == 0 || W == 0){
             return 0;
         }
+        
         if(t[n][W] != -1){
             return t[n][W];
         }
-        
-        if(wt[n-1] <= W){
-            return t[n][W] = max(val[n-1] + knapsackRec(W - wt[n-1], val, wt, n-1, t), knapsackRec(W, val, wt, n-1, t));
+        else if(wt[n-1] <= W){
+            return t[n][W] = max(val[n-1] + knapsackrec(W-wt[n-1], val, wt, n-1, t), knapsackrec(W, val, wt, n-1, t));
         }
         else if(wt[n-1] > W){
-            return t[n][W] = knapsackRec(W, val, wt, n-1, t);
+            return t[n][W] = knapsackrec(W, val, wt, n-1, t);
         }
     }
+  public:
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
-        
         // code here
-        
         int n = val.size();
         
-        vector<vector<int>> t(n + 1, vector<int>(W + 1, -1));
+        vector<vector<int>> t(n+1, vector<int>(W+1, -1));
         
-        
-        return knapsackRec(W, val, wt, n, t);
+        return knapsackrec(W, val, wt, n,t);
         
     }
 };
