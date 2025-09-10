f4c9b922e9deedecb8a98f21b6351305863c7af6
Amit Gunjan Jha <119215172+imjhaji03@users.noreply.github.com>
2025-09-10 18:58:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
@@ -1,40 +1,33 @@
 class Solution {
   public:
-    void BFSG(unordered_map<int, vector<int>>&adj, int u, vector<bool> &visited, vector<int> & result){
-        queue<int> q;
-        q.push(u);
-        visited[u]=true;
-        result.push_back(u);
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfs(vector<vector<int>> &adj) {
+        // Code here
+        int V = (int)adj.size();
+        vector<int> bfs;
+        vector<int> visvec(V, 0);
         
-        while(!q.empty()){
-            int u = q.front();
-            q.pop();
-            for(auto &v : adj[u]){
-                if(!visited[v]){
-                    q.push(v);
-                    visited[v]=true;
-                    result.push_back(v);
+        int *vis = visvec.data();
+        
+        
+        for(int i = 0; i < V; i++){
+            if(vis[i]) continue;
+            vis[i] = 1;
+            queue<int> q;
+            q.push(i);
+            while(!q.empty()){
+                int node = q.front();
+                q.pop();
+                bfs.push_back(node);
+                for(auto it : adj[node]){
+                    if(!vis[it]){
+                        vis[it] = 1;
+                        q.push(it);
+                    }
                 }
             }
         }
-    }
-  public:
-    // Function to return Breadth First Traversal of given graph.
-    vector<int> bfs(vector<vector<int>> &mpp) {
-        // Code here
-        int  V = mpp.size();
-        vector<int> result;
-        unordered_map<int, vector<int>> adj;
-        for(int u = 0; u < V; u++){
-            for(auto &it : mpp[u]){
-                adj[u].push_back(it);
-            }
-        }
-        
-        vector<bool> visited(V,false);
-        BFSG(adj, 0, visited  ,result);
-        return result;
-        
+        return bfs;
         
     }
 };
