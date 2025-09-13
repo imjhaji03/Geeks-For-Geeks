21a1f2a3e94f166ebb785e02debb596cf8a64d1d
Amit Gunjan Jha <119215172+imjhaji03@users.noreply.github.com>
2025-09-13 10:43:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.cpp
+++ Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.cpp
@@ -1,52 +1,26 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
 class Solution {
   public:
     bool twoSum(vector<int>& arr, int target) {
-        for (int i = 0; i < arr.size(); i++) {
-            for (int j = i + 1; j < arr.size(); j++) {
-                if (arr[i] + arr[j] == target) {
-                    return true; 
-                }
+        // code here
+        int n = arr.size();
+        int i = 0;
+        int j = n-1;
+        
+        sort(arr.begin(),arr.end());
+        
+        while(i < j){
+            if(arr[i] + arr[j] > target){
+                j--;
+            }
+            else if(arr[i] + arr[j] < target){
+                i++;
+            }
+            else{
+                return true;
             }
         }
+        
         return false;
+        
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // To discard any leftover newline characters
-    while (t--) {
-        int x;
-        cin >> x;
-        cin.ignore(); // To discard any leftover newline characters
-
-        vector<int> arr;
-        string input;
-        getline(cin, input); // Read the entire line for the array elements
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        auto ans = ob.twoSum(arr, x);
-        cout << (ans ? "true" : "false") << endl;
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
