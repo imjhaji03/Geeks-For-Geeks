580813a675cdbb8b99ca0acf63ac411a7f1fb006
Amit Gunjan Jha <119215172+imjhaji03@users.noreply.github.com>
2026-02-14 22:12:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Wave Array/wave-array.cpp
+++ Difficulty: Medium/Wave Array/wave-array.cpp
@@ -2,9 +2,7 @@ class Solution {
   public:
     void sortInWave(vector<int>& arr) {
         // code here
-        int n = arr.size();
-        
-        for(int i = 0; i < n-1; i += 2){
+        for(int i = 0; i < arr.size()-1; i= i+2){
             swap(arr[i],arr[i+1]);
         }
     }
