bb1b0fcf2192e38188edfde4fa09386bc562199d
Amit Gunjan Jha <119215172+imjhaji03@users.noreply.github.com>
2026-02-14 23:31:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Remove Duplicates Sorted Array/remove-duplicates-sorted-array.cpp
+++ Difficulty: Easy/Remove Duplicates Sorted Array/remove-duplicates-sorted-array.cpp
@@ -1,21 +1,17 @@
 class Solution {
   public:
-    // Function to remove duplicates from the given array.
     vector<int> removeDuplicates(vector<int> &arr) {
-        // code here
-        vector<int> res;
-        set<int> s;
+        vector<int> temp;
         
-        for(int i = 0; i < arr.size(); i++){
-            s.insert(arr[i]);
-            
+        temp.push_back(arr[0]);
+        
+        for(int i = 1; i < arr.size(); i++){
+            if(arr[i] != arr[i-1]){
+                temp.push_back(arr[i]);
+            }
         }
         
-        for(auto it : s){
-            res.push_back(it);
-        }
-        
-        return res;
+        return temp;
         
     }
 };
